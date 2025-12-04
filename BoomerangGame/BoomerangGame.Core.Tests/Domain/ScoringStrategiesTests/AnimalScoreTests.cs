23058c3807de357363aef65d357c8760dbb2f32c
a21jeaha <hisui86@gmail.com>
2025-12-04 15:55:33 +0100

    created AnimalScore and associated tests

---
+++ BoomerangGame/BoomerangGame.Core.Tests/Domain/ScoringStrategiesTests/AnimalScoreTests.cs
@@ -0,0 +1,264 @@
+﻿using BoomerangGame.Core.Domain.Cards;
+using BoomerangGame.Core.Domain.ScoringStrategies;
+using BoomerangGame.Core.Domain.States.PlayerState;
+using BoomerangGame.Core.Tests.HelperClasses;
+using Moq;
+
+namespace BoomerangGame.Core.Tests.Domain.ScoringStrategiesTests;
+
+public class AnimalScoreTests
+{
+	private IScoreCategory _sut;
+
+	private MockBoomerangPlayerStateCreator _mockBoomerangPlayerStateCreator;
+	private MockCardCreator _mockCardCreator;
+	private Mock<IBoomerangPlayerState> _mockBPState;
+	private const string _c_PLAYER_NAME = "TestPlayer";
+
+	public AnimalScoreTests()
+	{
+		_sut = new AnimalScore();
+
+		_mockBoomerangPlayerStateCreator = new MockBoomerangPlayerStateCreator();
+		_mockBPState = _mockBoomerangPlayerStateCreator.CreateMockBoomerangPlayerState(_c_PLAYER_NAME);
+
+		_mockCardCreator = new MockCardCreator();
+	}
+
+	[Fact]
+	public void CalculateScore_PlayerStateNull_ThrowsArgumentNullException()
+	{
+		// Act & Assert
+		Assert.Throws<ArgumentNullException>(() =>
+			_sut.CalculateScore(null!)
+		);
+	}
+
+	[Fact]
+	public void CalculateScore_DraftedCardsNull_ThrowsInvalidOperationException()
+	{
+		// Arrange
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns((List<IBoomerangCard<string>>?)null!);
+
+		// Act & Assert
+		Assert.Throws<InvalidOperationException>(() =>
+			_sut.CalculateScore(_mockBPState.Object)
+		);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_OnlyKangaroos_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Kangaroos",
+			"animal", "Kangaroos",
+			"animal", "Kangaroos",
+			"animal", "Kangaroos",
+			"animal", "Kangaroos",
+			"animal", "Kangaroos",
+			"animal", "Kangaroos"
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(9, result);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_OnlyEmus_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Emus",
+			"animal", "Emus",
+			"animal", "Emus",
+			"animal", "Emus",
+			"animal", "Emus",
+			"animal", "Emus",
+			"animal", "Emus"
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(12, result);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_OnlyWombats_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Wombats",
+			"animal", "Wombats",
+			"animal", "Wombats",
+			"animal", "Wombats",
+			"animal", "Wombats",
+			"animal", "Wombats",
+			"animal", "Wombats"
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(15, result);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_OnlyKoalas_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Koalas",
+			"animal", "Koalas",
+			"animal", "Koalas",
+			"animal", "Koalas",
+			"animal", "Koalas",
+			"animal", "Koalas",
+			"animal", "Koalas"
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(21, result);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_OnlyPlatypuses_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Platypuses",
+			"animal", "Platypuses",
+			"animal", "Platypuses",
+			"animal", "Platypuses",
+			"animal", "Platypuses",
+			"animal", "Platypuses",
+			"animal", "Platypuses"
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(27, result);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_MixedPairs_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Platypuses",
+			"animal", "Emus",
+			"animal", "Kangaroos",
+			"animal", "Platypuses",
+			"animal", "Emus",
+			"animal", "Kangaroos",
+			"animal", "Koalas "
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(16, result);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_TwoPairs_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Platypuses",
+			"animal", "Emus",
+			"animal", "Kangaroos",
+			"animal", "Platypuses",
+			"animal", "Wombats",
+			"animal", "Kangaroos",
+			"animal", "Koalas "
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(12, result);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_OnePairs_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Platypuses",
+			"animal", "Emus",
+			"animal", "Fish",
+			"animal", "Platypuses",
+			"animal", "Wombats",
+			"animal", "Kangaroos",
+			"animal", "Koalas "
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(9, result);
+	}
+
+	[Fact]
+	public void CalculateScore_ValidDraftedCards_NoPairs_ReturnsCorrectScore()
+	{
+		// Arrange
+		List<IBoomerangCard<string>> mockDraftedCards =
+			_mockCardCreator.CreateFullSetOfMockCardWithCollectionSymbol(
+			"animal", "Mouse",
+			"animal", "Emus",
+			"animal", "Fish",
+			"animal", "Platypuses",
+			"animal", "Wombats",
+			"animal", "Kangaroos",
+			"animal", "Koalas "
+		);
+
+		_mockBPState.SetupGet(bPS => bPS.DraftedCards).Returns(mockDraftedCards);
+
+		// Act
+		int result = _sut.CalculateScore(_mockBPState.Object);
+
+		// Assert
+		Assert.Equal(0, result);
+	}
+}
---
+++ BoomerangGame/BoomerangGame.Core/Domain/ScoringStrategies/AnimalScore.cs
@@ -0,0 +1,58 @@
+﻿using BoomerangGame.Core.Domain.ScoringStrategies.Utilities;
+using BoomerangGame.Core.Domain.States.PlayerState;
+
+namespace BoomerangGame.Core.Domain.ScoringStrategies;
+
+/// <summary>
+/// Represents a scoring strategy that calculates points based on drafted animal cards. Points are <br/>
+/// awarded for pairs of the same animal type, with different animal types having different point values.
+/// </summary>
+public class AnimalScore : IScoreCategory
+{
+	public int CalculateScore(IBoomerangPlayerState playerState)
+	{
+		ScoringStrategyPreConditionNullCheck.Check(playerState);
+
+		if (playerState.DraftedCards is null)
+			throw new InvalidOperationException("Player does not have a drafted cards");
+
+		Dictionary<string, int> countedDictionerty =
+			SymbolInstanceCounter.CountInstances(playerState.DraftedCards, "animal");
+
+		return CalculatePoints(countedDictionerty); 
+	}
+
+
+	private int CalculatePoints(Dictionary<string, int> countedDictionerty)
+	{
+		int points = 0;
+
+		foreach (KeyValuePair<string, int> collectionType in countedDictionerty)
+		{
+			switch (collectionType.Key)
+			{
+				case "Kangaroos":
+					points += 3 * GetNrOfPairs(collectionType.Value);
+					break;
+				case "Emus":
+					points += 4 * GetNrOfPairs(collectionType.Value);
+					break;
+				case "Wombats":
+					points += 5 * GetNrOfPairs(collectionType.Value);
+					break;
+				case "Koalas":
+					points += 7 * GetNrOfPairs(collectionType.Value);
+					break;
+				case "Platypuses":
+					points += 9 * GetNrOfPairs(collectionType.Value);
+					break;
+				default:
+					break;
+			}
+		}
+
+		return points;
+	}
+
+	private int GetNrOfPairs(int value) => value / 2; 
+}
