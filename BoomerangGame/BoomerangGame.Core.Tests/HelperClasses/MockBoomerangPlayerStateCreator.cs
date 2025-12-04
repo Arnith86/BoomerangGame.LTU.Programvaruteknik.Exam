5208a4bcd1a2b20ee827257c2ba05b5410f1d325
a21jeaha <hisui86@gmail.com>
2025-12-04 08:28:20 +0100

    added  BoomerangPlayerState and RoundState Mock Creators

---
+++ BoomerangGame/BoomerangGame.Core.Tests/HelperClasses/MockBoomerangPlayerStateCreator.cs
@@ -0,0 +1,16 @@
+﻿using BoomerangGame.Core.Domain.States.PlayerState;
+using Moq;
+
+namespace BoomerangGame.Core.Tests.HelperClasses;
+
+public class MockBoomerangPlayerStateCreator
+{
+	
+	public Mock<IBoomerangPlayerState>CreateMockBoomerangPlayerState(string playerName)
+	{
+		Mock<IBoomerangPlayerState> mock = new Mock<IBoomerangPlayerState>();
+		mock.SetupGet(bPS => bPS.Name).Returns(playerName);
+
+		return mock;
+	}
+}
---
+++ BoomerangGame/BoomerangGame.Core.Tests/HelperClasses/MockRoundStateCreator.cs
@@ -0,0 +1,31 @@
+﻿using BoomerangGame.Core.Application;
+using BoomerangGame.Core.Domain.Cards;
+using BoomerangGame.Core.Domain.States.RoundStates;
+using Moq;
+
+namespace BoomerangGame.Core.Tests.HelperClasses;
+
+public class MockRoundStateCreator
+{
+	public Mock<IRoundState> CreateMockRoundState(
+	IPlayer player,
+	IBoomerangCard? throwCard = null,
+	IBoomerangCard? catchCard = null)
+	{
+		var mock = new Mock<IRoundState>();
+
+		var throwDict = new Dictionary<string, IBoomerangCard>();
+		var catchDict = new Dictionary<string, IBoomerangCard>();
+
+		if (throwCard != null)
+			throwDict[player.PlayerState.Name] = throwCard;
+
+		if (catchCard != null)
+			catchDict[player.PlayerState.Name] = catchCard;
+
+		mock.Setup(r => r.ThrowCards).Returns(throwDict);
+		mock.Setup(r => r.CatchCards).Returns(catchDict);
+
+		return mock;
+	}
+}
